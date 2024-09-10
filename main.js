883fd3a0970aace5781eeaca01babbfe810e211c
ivantarabanovych <vanjokcitet334@gmail.com>
2024-09-10 23:21:40 +0300

    q5

--- main.js
+++ main.js
@@ -3,13 +3,17 @@ const book = {
     author: "Kulyk Stepan"
 };
 
+
+Object.preventExtensions(book);
+book.pages = 350;
+console.log(book.pages);
+
 Object.freeze(book);
 
 book.title = "Harry Potter and the Philosopher's Stone"
 console.log(book.title);
 
-book.pages = 350;
-console.log(book.pages);
+Object.seal(book);
 
 delete book.author;
 console.log(book);
